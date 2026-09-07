31f43810130bee0bc23a3ee9773e7f6f90a0e0a2
sy-sugawara <sy-sugawara@okamoto-group.co.jp>
2026-09-07 14:45:31 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ if st.button("128x192 PNG画像を生成"):
     # 3. 文字間隔を調整しながら1文字ずつ描画
     start_x = 0          # 1文字目の描画開始X座標
     y_pos = 105          # Y座標
-    letter_spacing = -8  # 文字間隔（マイナス値にすると字間が詰まります）
+    letter_spacing = -6  # 文字間隔（マイナス値にすると字間が詰まります）
 
     current_x = start_x
     for char in price_int:
